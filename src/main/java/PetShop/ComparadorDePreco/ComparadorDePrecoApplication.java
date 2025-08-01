34766af130ea6a79182d2845aa3d82728acde4c6
EstevaoA98 <estevao.a98@gmail.com>
2025-08-01 15:36:27 -0300

    Initial commit

---
+++ src/main/java/PetShop/ComparadorDePreco/ComparadorDePrecoApplication.java
@@ -0,0 +1,13 @@
+package PetShop.ComparadorDePreco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComparadorDePrecoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComparadorDePrecoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/PetShop/ComparadorDePreco/ComparadorDePrecoApplicationTests.java
@@ -0,0 +1,13 @@
+package PetShop.ComparadorDePreco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComparadorDePrecoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
