7367167ae2611cb5d4eb24c4717b8c0584bb8d9c
EstevaoA98 <estevao.a98@gmail.com>
2025-08-02 13:53:12 -0300

    feat: implementar serviço para calcular melhor petshop com base em preço e distância

--- src/main/java/PetShop/ComparadorDePreco/Service/PetShopService.java
+++ src/main/java/PetShop/ComparadorDePreco/Service/PetShopService.java
@@ -1,7 +1,51 @@
 package PetShop.ComparadorDePreco.Service;
 
+import PetShop.ComparadorDePreco.DTO.CotacaoBanhoDTO;
+import PetShop.ComparadorDePreco.Model.PetShop;
+import PetShop.ComparadorDePreco.Model.PrecoBanho;
 import org.springframework.stereotype.Service;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.util.*;
 
 @Service
 public class PetShopService {
+
+    public Map<String, Object> cotacaoBanho(CotacaoBanhoDTO dto) {
+        boolean finalDeSemana = isFinalDeSemana(dto.getData());
+        int pequenos = dto.getCachorroPequeno();
+        int grandes = dto.getCachorroGrande();
+
+        List<PetShop> petShops = List.of(
+                new PetShop("Meu Canino Feliz", new PrecoBanho(20, 40, 24, 48), 2.0),
+                new PetShop("Vai Rex", new PrecoBanho(15, 50, 20, 55), 1.7),
+                new PetShop("ChowChawgas", new PrecoBanho(30, 45, 30, 45), 0.8)
+        );
+
+        PetShop melhorPetShop = null;
+        double melhorPreco = Double.MAX_VALUE;
+
+        for (PetShop petShop : petShops) {
+            double preco = petShop.calculoValorPetshop(finalDeSemana, pequenos, grandes);
+
+            boolean precoMelhor = preco < melhorPreco;
+            boolean mesmaPrecoMenorDist = preco == melhorPreco && petShop.getDistancia() < melhorPetShop.getDistancia();
+
+            if (melhorPetShop == null || precoMelhor || mesmaPrecoMenorDist) {
+                melhorPetShop = petShop;
+                melhorPreco = preco;
+            }
+        }
+
+        return Map.of(
+                "nomePetshop", melhorPetShop.getNome(),
+                "precoTotal", melhorPreco
+        );
+    }
+
+    private boolean isFinalDeSemana(LocalDate data) {
+        DayOfWeek dia = data.getDayOfWeek();
+        return dia == DayOfWeek.SATURDAY || dia == DayOfWeek.SUNDAY;
+    }
 }
