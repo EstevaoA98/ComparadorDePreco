1901144bf63506a0b6f06a9c9f5f55d0f6e52895
EstevaoA98 <estevao.a98@gmail.com>
2025-08-02 09:57:41 -0300

    refactor: colocando os preços na classe PrecoBanho

--- src/main/java/PetShop/ComparadorDePreco/Model/PetShop.java
+++ src/main/java/PetShop/ComparadorDePreco/Model/PetShop.java
@@ -5,18 +5,12 @@ import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
-
 public class PetShop {
     private String nome;
-    private double valorBanhoCachorroPequenoSemana;
-    private double valorBanhoCachorroGrandeSemana;
-    private double valorBanhoPequenoFinalDeSemana;
-    private double valorBanhoGrandeFinalDeSemana;
+    private PrecoBanho precos;
     private double distancia;
 
-    public double CalculoValorPetshop(Boolean finalDeSemana, int Pequeno, int Grande) {
-        double ValorPequeno = finalDeSemana ? valorBanhoPequenoFinalDeSemana : valorBanhoCachorroPequenoSemana;
-        double ValorGrande = finalDeSemana ? valorBanhoGrandeFinalDeSemana : valorBanhoCachorroGrandeSemana;
-        return (Pequeno * ValorPequeno) + (Grande * ValorGrande);
-            }
+    public double calculoValorPetshop(boolean finalDeSemana, int qtdPequenos, int qtdGrandes) {
+        return precos.calcularPrecoTotal(finalDeSemana, qtdPequenos, qtdGrandes);
+    }
 }
--- src/main/java/PetShop/ComparadorDePreco/Model/PrecoBanho.java
+++ src/main/java/PetShop/ComparadorDePreco/Model/PrecoBanho.java
@@ -1,4 +1,19 @@
 package PetShop.ComparadorDePreco.Model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class PrecoBanho {
+    private double precoSemanaPequeno;
+    private double precoSemanaGrande;
+    private double precoFinalDeSemanaPequeno;
+    private double precoFinalDeSemanaGrande;
+
+    public double calcularPrecoTotal(boolean finalDeSemana, int qtdPequenos, int qtdGrandes) {
+        double precoPequeno = finalDeSemana ? precoFinalDeSemanaPequeno : precoSemanaPequeno;
+        double precoGrande = finalDeSemana ? precoFinalDeSemanaGrande : precoSemanaGrande;
+        return (qtdPequenos * precoPequeno) + (qtdGrandes * precoGrande);
+    }
 }
