5e85ba9a17cadc60c4d39e157da8f233147feb94
EstevaoA98 <estevao.a98@gmail.com>
2025-08-02 13:55:08 -0300

    feat: implementa endpoint para cotação de banho no petshop

--- src/main/java/PetShop/ComparadorDePreco/Controller/PetShopController.java
+++ src/main/java/PetShop/ComparadorDePreco/Controller/PetShopController.java
@@ -1,7 +1,21 @@
 package PetShop.ComparadorDePreco.Controller;
 
-import org.springframework.stereotype.Controller;
+import PetShop.ComparadorDePreco.Service.PetShopService;
+import PetShop.ComparadorDePreco.DTO.CotacaoBanhoDTO;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
+@RequestMapping("/petshop")
 public class PetShopController {
+
+    @Autowired
+    private PetShopService service;
+
+    @PostMapping("/cotacao")
+    public ResponseEntity<?> cotar(@Valid @RequestBody CotacaoBanhoDTO dto) {
+        return ResponseEntity.ok(service.cotacaoBanho(dto));
+    }
 }
