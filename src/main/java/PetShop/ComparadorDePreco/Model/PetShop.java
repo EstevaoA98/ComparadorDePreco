a379c7c77327fb99d40aad69625406313722449e
EstevaoA98 <estevao.a98@gmail.com>
2025-08-01 21:40:49 -0300

    feat: cria model PetShop com preços e cálculo por tipo de cão e dia da semana

--- src/main/java/PetShop/ComparadorDePreco/Model/PetShop.java
+++ src/main/java/PetShop/ComparadorDePreco/Model/PetShop.java
@@ -1,4 +1,22 @@
 package PetShop.ComparadorDePreco.Model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+
 public class PetShop {
+    private String nome;
+    private double valorBanhoCachorroPequenoSemana;
+    private double valorBanhoCachorroGrandeSemana;
+    private double valorBanhoPequenoFinalDeSemana;
+    private double valorBanhoGrandeFinalDeSemana;
+    private double distancia;
+
+    public double CalculoValorPetshop(Boolean finalDeSemana, int Pequeno, int Grande) {
+        double ValorPequeno = finalDeSemana ? valorBanhoPequenoFinalDeSemana : valorBanhoCachorroPequenoSemana;
+        double ValorGrande = finalDeSemana ? valorBanhoGrandeFinalDeSemana : valorBanhoCachorroGrandeSemana;
+        return (Pequeno * ValorPequeno) + (Grande * ValorGrande);
+            }
 }
