246391413cea37dfc6c1f2e74b4223d328c339c2
EstevaoA98 <estevao.a98@gmail.com>
2025-08-03 09:37:57 -0300

    feat: implementar método para listar informações de todos os petshops

--- src/main/java/PetShop/ComparadorDePreco/Service/PetShopService.java
+++ src/main/java/PetShop/ComparadorDePreco/Service/PetShopService.java
@@ -4,24 +4,44 @@ import PetShop.ComparadorDePreco.DTO.CotacaoBanhoDTO;
 import PetShop.ComparadorDePreco.Model.PetShop;
 import PetShop.ComparadorDePreco.Model.PrecoBanho;
 import org.springframework.stereotype.Service;
+import PetShop.ComparadorDePreco.DTO.PetShopInformacaosDTO;
 
 import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 public class PetShopService {
+
+    private final List<PetShop> petShops;
+
+    public PetShopService() {
+        this.petShops = List.of(
+                new PetShop("Meu Canino Feliz", new PrecoBanho(20, 40, 24, 48), 2.0),
+                new PetShop("Vai Rex", new PrecoBanho(15, 50, 20, 55), 1.7),
+                new PetShop("ChowChawgas", new PrecoBanho(30, 45, 30, 45), 0.8)
+        );
+    }
+
+    public List<PetShopInformacaosDTO> getAllPetShopsInfo() {
+        return petShops.stream()
+                .map(petShop -> new PetShopInformacaosDTO(
+                        petShop.getNome(),
+                        petShop.getPrecos().getPrecoSemanaPequeno(),
+                        petShop.getPrecos().getPrecoSemanaGrande(),
+                        petShop.getPrecos().getPrecoFinalDeSemanaPequeno(),
+                        petShop.getPrecos().getPrecoFinalDeSemanaGrande(),
+                        petShop.getDistancia()
+                ))
+                .collect(Collectors.toList());
+    }
 
     public Map<String, Object> cotacaoBanho(CotacaoBanhoDTO dto) {
         boolean finalDeSemana = isFinalDeSemana(dto.getData());
         int pequenos = dto.getCachorroPequeno();
         int grandes = dto.getCachorroGrande();
 
-        List<PetShop> petShops = List.of(
-                new PetShop("Meu Canino Feliz", new PrecoBanho(20, 40, 24, 48), 2.0),
-                new PetShop("Vai Rex", new PrecoBanho(15, 50, 20, 55), 1.7),
-                new PetShop("ChowChawgas", new PrecoBanho(30, 45, 30, 45), 0.8)
-        );
 
         PetShop melhorPetShop = null;
         double melhorPreco = Double.MAX_VALUE;
